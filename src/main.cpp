5d56f70cbd0b1ff5023fee65dc4a3e41f24ec08c
WormEmperor <poqiwuer@gmail.com>
2023-01-04 16:44:29 +0200

    remade in QT

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,11 @@
-#include <gtkmm.h>
-#include <iostream>
-#include <chrono>
-#include <thread>
-
-class MyWindow : public Gtk::Window {
-    public:
-        MyWindow();
-};
-
-MyWindow::MyWindow() {
-    set_title("Taimen");
-    set_default_size(640, 480);
-}
+#include <QApplication>
+#include <QPushButton>
 
 int main(int argc, char* argv[]) {
-    auto app = Gtk::Application::create("org.gtkmm.examples.base");
-    return app->make_window_and_run<MyWindow>(argc, argv);
+    QApplication app(argc, argv);
+
+    QPushButton button("Hello World!");
+    button.show();
+
+    return app.exec();
 }
