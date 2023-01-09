8c3011389ff7d0facb0652caa479a998aab917cf
WormEmperor <poqiwuer@gmail.com>
2023-01-09 17:52:32 +0200

    more changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,27 @@
-#include "main_window.hpp"
 #include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsTextItem>
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
 
-    mainWindow window;
+    QGraphicsScene scene;
+    scene.setBackgroundBrush(Qt::black);
+    QGraphicsTextItem* text1 = scene.addText("Hello");
+    QGraphicsTextItem* text2 = scene.addText("Hello again");
+
+    text1->setDefaultTextColor(Qt::white);
+
+    text2->setPos(10, 10);
+
+    QGraphicsView view(&scene);
     
 //    const static int width = QCoreApplication::arguments().at(1).toInt();
 //    const static int height = QCoreApplication::arguments().at(2).toInt();
 //    window.setFixedSize(width, height);
 
-    window.show();
+    view.show();
 
     return app.exec();
 }
--- src/taimen.pro
+++ src/taimen.pro
@@ -5,5 +5,6 @@ QT = core gui
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
-SOURCES += main.cpp main_window.cpp
-HEADERS += main_window.hpp 
+SOURCES += main.cpp
+#HEADERS += main_window.hpp 
+
